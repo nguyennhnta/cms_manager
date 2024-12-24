d921a6e78a6808bd255b286405a9d26c406ce28f
nguyennhdn <nguyennhdn@gmail.com>
2024-12-24 10:12:05 +0700

    add

--- laravel-cms/app/Http/Controllers/PostController.php
+++ laravel-cms/app/Http/Controllers/PostController.php
@@ -14,35 +14,58 @@ class PostController extends Controller implements  HasMiddleware
     {
         return [
 //            'role_or_permission:manager|edit articles', //Kiểm tra nếu người dùng có vai trò hoặc quyền cụ thể.
-//            new Middleware('role:Viewer', only: ['index']), //Kiểm tra vai trò Admin
+            new Middleware('role:admin', only: ['store', 'udpate', 'destroy']), //Kiểm tra vai trò Admin
+//            new Middleware('role:viewer', only: ['index']), //Kiểm tra vai trò Admin
 //            new Middleware(\Spatie\Permission\Middleware\RoleMiddleware::using('manager'), except:['show']), // Sử dụng middleware role với vai trò manager.
 //            new Middleware(\Spatie\Permission\Middleware\PermissionMiddleware::using('delete records,api'), only:['destroy']), //Kiểm tra quyền delete records và api.
         ];
     }
 
-    public function index()
-    {
-//        $allUsersWithAllTheirRoles = User::with('roles')->get();
-//        return response()->json($allUsersWithAllTheirRoles, 201);
-//        return Post::all();
-        $user = User::find(1);
-        $user->assignRole('admin');
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function index() {
+        return Post::all();
     }
 
-    public function store(Request $request)
-    {
+    /**
+     * @param Post $post
+     * @return mixed
+     */
+    public function show(Post $post) {
+        if (!auth()->user()->hasAnyRole(['editor', 'admin'])) {
+            return response()->json([
+                'message' => 'Bạn không có quyền truy cập bài viết này.',
+                'status' => 403
+            ], 403);
+        }
+        return response()->json($post);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function store(Request $request) {
         $post = Post::create($request->all());
         return response()->json($post, 201);
     }
 
-    public function update(Request $request, Post $post)
-    {
+    /**
+     * @param Request $request
+     * @param Post $post
+     * @return mixed
+     */
+    public function update(Request $request, Post $post) {
         $post->update($request->all());
         return response()->json($post);
     }
 
-    public function destroy(Post $post)
-    {
+    /**
+     * @param Post $post
+     * @return mixed
+     */
+    public function destroy(Post $post) {
         $post->delete();
         return response()->json(null, 204);
     }
