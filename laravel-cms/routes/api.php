53fb4945b63951c3d203db5d070743ba74230e74
nguyennhdn <nguyennhdn@gmail.com>
2024-12-31 15:38:47 +0700

    add

--- laravel-cms/routes/api.php
+++ laravel-cms/routes/api.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:api']], function() {
-    Route::post('user', [AuthController::class, 'user']);
+    Route::get('user', [AuthController::class, 'user']);
     Route::resource('products', ProductController::class);
     Route::get('get_user_role', [AuthController::class, 'getUserRoles']);
 });
@@ -24,3 +24,5 @@ Route::middleware('auth:api')->get('/auth/check-token', function () {
 //Route::middleware(['role:Admin'])->group(function () {
 //    Route::get('/admin', [AdminController::class, 'index']);
 //});
+Route::get('auth/google', [AuthController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
